a24a5075e94a82e34e309511706bd9899920237d
quicoju <quicoju@hp.home>
2025-09-24 16:03:33 +0200

    add hch test for the "cancel-reservation" command
    This change required to support multi-line regex, which was
    done by replacing the new line character  with the '|';
    then match this new character in the regular expression.

--- t/t_hch.bash
+++ t/t_hch.bash
@@ -8,7 +8,7 @@ run_test() {
     echo -n "Test $test_name ..."
     local got=$(echo -e "$input" | ./hch 2>&1)
 
-    if echo "$got" | grep -q -- "$expected"; then
+    if echo "$got" | tr '\n' '|' | grep -q -- "$expected"; then
         echo "ok"
         return 0
     else
@@ -47,3 +47,14 @@ run_test "List reservations shows period" \
  list-reservations
  quit" \
 "- \[2024-Dec-01/2024-Dec-03\]"
+
+run_test "Cancel reservation" \
+"set-room room-101
+ reserve 2024-12-01+3d
+ reserve 2025-01-05+5d
+ cancel-reservation 2024-12-02
+ list-reservations
+ quit" \
+"Reservation cancelled.\|.* \
+ - \[2025-Jan-05/2025-Jan-09\]"
+
