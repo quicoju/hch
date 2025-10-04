304ea0c4331d38c10d053e39633db5be93d49cba
quicoju <fjurado@cimat.mx>
2025-10-04 10:14:11 +0200

    factor out the "Repl" logic from "hch"
    This change improves the re-usability of "Repl", isolates the
    application logic and improves the backend resolution.

--- src/hch.cc
+++ src/hch.cc
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <regex>
-#include <stdexcept>
 #include <sstream>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
@@ -10,44 +10,25 @@
 
 #include "Hotel.hh"
 
-// Program default configuration
-struct Config {
-  std::string db_path = "db/hotel.db";
-};
-
-Config parse_args(int argc, char *argv[]) {
-  Config config;
-
-  for (auto i{1}; i < argc; i++) {
-    std::string arg{argv[i]};
-    if (arg.find("--db=") == 0)
-      config.db_path = arg.substr(5);
-  }
-  return config;
-}
-
-// Parse date string like "2025-11-03" or "2025-11-03+3d"
-std::pair<Date, Duration> parse_date(const std::string &s)
-{
-  using namespace std;
-  regex re(R"((\d{4}-\d{2}-\d{2})(?:\+(\d+)d)?)");
-  smatch match;
-
-  if (!regex_match(s, match, re))
-    throw std::invalid_argument{"Invalid date format"};
-
-  Date date{from_string(match[1])};
-  Duration duration{match[2].matched ? stoi(match[2]) : 1};
-
-  return {date, duration};
-}
+#include "backend/memory/HotelData.hh"
+#ifdef USE_sqlite
+#include "backend/sqlite/SQLite.hh"
+using Backend = SQLite;
+#else
+using Backend = HotelData;
+struct SQLite; // mock unused backend
+#endif
 
 struct Repl {
-  Repl(Hotel& h) : hotel{h} {}
+  using string = std::string;
+  using Tokens = std::vector<string>;
+
+  virtual string prompt() const = 0;
+  virtual void execute(const string&, const Tokens&) = 0;
 
   void run() {
     while (true) {
-      char *line = readline(get_prompt().c_str());
+      char *line = readline(prompt().c_str());
       if (!line) break;              /* EOF */
       if (*line) add_history(line);  /* don't keep empty lines */
       execute_command(tokenize(line));
@@ -56,20 +37,6 @@ struct Repl {
   }
 
 private:
-
-  using string = std::string;
-  using Tokens = std::vector<string>;
-
-  Hotel& hotel;
-  string current_room;
-  string command;
-
-  string get_prompt() const {
-    return current_room.empty()
-      ? "(hch) "
-      : "(hch " + current_room + ") ";
-  }
-
   Tokens tokenize(const string &line) {
     std::istringstream iss(line);
     string token;
@@ -81,53 +48,48 @@ private:
     return tokens;
   }
 
-  Room ensure_room(const Tokens& tokens) {
-    string room_id{current_room};
+  void execute_command(const Tokens& t) {
+    if (t.empty()) return;
+    string command = t[0];
+    execute(command, t);
+  }
+};
 
-    if (room_id.empty() && tokens.size() >= 2)
-      room_id = tokens[1];
+template<typename T=Backend>
+struct Hch : Repl {
+  Hch(int argc, char* argv[])
+    : conf{ argc, argv }
+    , src{ get_source() }
+    , hotel{ Hotel{&src} }
+  { }
 
-    if (room_id.empty()) throw std::invalid_argument{
-        string{"Command usage: "} + command + " ROOM"};
-
-    return hotel.room(room_id);
+  string prompt() const override {
+    return current_room.empty()
+      ? "(hch) "
+      : "(hch " + current_room + ") ";
   }
 
-  void execute_command(const Tokens &tokens) {
-    if (tokens.empty()) return;
-
-    command = string{tokens[0]};
-
+  void execute(const string& command, const Tokens& tokens) override {
     try {
       if (command == "set-room") {
         if (tokens.size() < 2) throw std::invalid_argument{
             "Command usage: set-room ROOM"};
         current_room = hotel.room(tokens[1]).id;
-        return;
       }
-
-      if (command == "unset-room") {
+      else if (command == "unset-room") {
         current_room.clear();
-        return;
       }
-
-      if (command == "list-reservations") {
-        auto r = ensure_room(tokens);
+      else if (command == "list-reservations") {
+        auto r = ensure_room(command, tokens);
         for (const auto& reservation : r.reservations())
           std::cout << "  - " << _pstr(reservation) << std::endl;
-
-        return;
       }
-
-      if (command == "list-amenities") {
-        auto r = ensure_room(tokens);
+      else if (command == "list-amenities") {
+        auto r = ensure_room(command, tokens);
         for (const auto& a : r.amenities())
           std::cout << "  - " << a << std::endl;
-
-        return;
       }
-
-      if (command == "reserve") {
+      else if (command == "reserve") {
         if (tokens.size() < 2) throw std::invalid_argument {
           "Command usage: reserve DATE[+DAYS] [ROOM]"};
 
@@ -143,10 +105,8 @@ private:
         auto [date, duration] = parse_date(date_str);
         hotel.room(room_id).reserve(date, duration);
         std::cout << "Room reserved successfully.\n";
-        return;
       }
-
-      if (command == "cancel-reservation") {
+      else if (command == "cancel-reservation") {
         if (tokens.size() < 2) throw std::invalid_argument {
           "Command usage: cancel-reservation DATE[+DAYS] [ROOM]"};
 
@@ -162,51 +122,88 @@ private:
         auto [date, _] = parse_date(date_str);
         hotel.room(room_id).cancel_reservation(date);
         std::cout << "Reservation cancelled.\n";
-        return;
       }
-
-      if (command == "quit" || command == "exit")
+      else if (command == "quit" || command == "exit") {
         std::exit(0);
-
-      std::cout << "Unknown command: " << command << std::endl;
+      }
+      else {
+        std::cout << "Unknown command: " << command << std::endl;
+      }
     }
     catch (const std::exception& e) {
       std::cout << "Error: " << e.what() << std::endl;
     }
   }
-};
+  
+private:
+  struct Config {
+    Config(int argc, char *argv[]) {
+      for (auto i{1}; i < argc; i++) {
+        std::string arg{argv[i]};
+        if (arg.find("--db=") == 0)
+          db_path = arg.substr(5);
+      }
+    }
+    string db_path = "db/hotel.db";
+  };
 
 
-#ifdef USE_sqlite
-#include "backend/sqlite/SQLite.hh"
-SQLite get_source(Config c) {
-  return {c.db_path};
-}
-#else
-#include "backend/memory/HotelData.hh"
-using namespace Amenity;
-HotelData get_source(Config c) {
-  return {
-    {"101", 1, {Wifi}},
-    {"102", 1, {Balcony, Wifi}, },
-    {"103", 1, {MiniBar, Wifi}, },
-    {"201", 1, {AirConditioning, Balcony}, },
-    {"202", 1, {Wifi}, },
-    {"203", 1, {MiniBar}, },
-   };
-}
-#endif
+  Room ensure_room(const string& command, const Tokens& tokens) {
+    string room_id{current_room};
+
+    if (room_id.empty() && tokens.size() >= 2)
+      room_id = tokens[1];
+
+    if (room_id.empty()) throw std::invalid_argument{
+        string{"Command usage: "} + command + " ROOM"};
+
+    return hotel.room(room_id);
+  }
+
+  // Parse date string like "2025-11-03" or "2025-11-03+3d"
+  std::pair<Date, Duration> parse_date(const std::string &s)
+  {
+    using namespace std;
+    regex re(R"((\d{4}-\d{2}-\d{2})(?:\+(\d+)d)?)");
+    smatch match;
+
+    if (!regex_match(s, match, re))
+      throw std::invalid_argument{"Invalid date format"};
+
+    Date date{from_string(match[1])};
+    Duration duration{match[2].matched ? stoi(match[2]) : 1};
+
+    return {date, duration};
+  }
+
+  auto get_source() {
+    using namespace Amenity;
+    if constexpr (std::is_same_v<T, SQLite>) {
+      return SQLite{conf.db_path};
+    }
+    else if constexpr (std::is_same_v<T, HotelData>)  {
+      return HotelData{
+        {"101", 1, {Wifi}},
+        {"102", 1, {Balcony, Wifi}, },
+        {"103", 1, {MiniBar, Wifi}, },
+        {"201", 1, {AirConditioning, Balcony}, },
+        {"202", 1, {Wifi}, },
+        {"203", 1, {MiniBar}, },
+      };
+    }
+  }
+
+  /* State */
+  Config conf;
+  Backend src;
+  Hotel hotel;
+  string current_room;
+}; // struct Hch 
+
 
 int main (int argc, char *argv[])
 {
-  auto config = parse_args(argc, argv);
-
-  // TODO: For now initialize a Hotel here
-  auto src = get_source(config);
-
-  Hotel hotel{&src};
-  Repl repl{hotel};
-  repl.run();
+  Hch<Backend>{argc, argv}.run();
 
   std::cout << "bye!\n";
   return 0;
