09dec5190e14281ed3f007463073239f0e59e2f4
quicoju <fjurado@cimat.mx>
2025-10-05 09:55:05 +0200

    build a convenient "HotelData" constructor

--- src/backend/memory/HotelData.hh
+++ src/backend/memory/HotelData.hh
@@ -13,8 +13,18 @@ struct RoomData {
 
 using RoomsData = std::vector<RoomData>;
 
+using namespace Amenity;
 struct HotelData {
-  HotelData() : rooms{} {}
+  HotelData(std::string _) : rooms{} {
+    rooms = {
+      {"101", 1, {Wifi}},
+      {"102", 1, {Balcony, Wifi}, },
+      {"103", 1, {MiniBar, Wifi}, },
+      {"201", 1, {AirConditioning, Balcony}, },
+      {"202", 1, {Wifi}, },
+      {"203", 1, {MiniBar}, },
+    };
+  }
   HotelData(RoomsData r) : rooms{r} {}
 
   // Properties
