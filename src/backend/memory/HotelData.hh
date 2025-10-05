7e58529bc26238d6d32877b8584c25453a5e48f3
quicoju <fjurado@cimat.mx>
2025-10-05 10:41:08 +0200

    add comment

--- src/backend/memory/HotelData.hh
+++ src/backend/memory/HotelData.hh
@@ -13,18 +13,25 @@ struct RoomData {
 
 using RoomsData = std::vector<RoomData>;
 
-using namespace Amenity;
 struct HotelData {
   HotelData(std::string _) : rooms{} {
+  // This constructor and data were made to feed
+  // hch's src default hotel, it takes a string
+  // as an argument to mirror the sqlite backend
+  // constructor, this helps to define a "Backend"
+  // type and make it very simple to build the backend
+  // specific source without using generic programming
+    using namespace Amenity;
     rooms = {
       {"101", 1, {Wifi}},
-      {"102", 1, {Balcony, Wifi}, },
-      {"103", 1, {MiniBar, Wifi}, },
-      {"201", 1, {AirConditioning, Balcony}, },
-      {"202", 1, {Wifi}, },
-      {"203", 1, {MiniBar}, },
+      {"102", 1, {Balcony, Wifi},},
+      {"103", 1, {MiniBar, Wifi},},
+      {"201", 1, {AirConditioning, Balcony},},
+      {"202", 1, {Wifi},},
+      {"203", 1, {MiniBar},}
     };
   }
+
   HotelData(RoomsData r) : rooms{r} {}
 
   // Properties
