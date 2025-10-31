69bd89abe4a088989636d1df84cfd8cd39aaadb0
quicoju <fjurado@cimat.mx>
2025-10-31 16:33:15 +0100

    attempt to factor out the logic to formatting the output
    It may be the case that the output is intended to a human user,
    or maybe to another service so the formatter will take care of
    serializing the output, so the application doesn't need to know
    about the details of where the output goes to.
    
    This first version of the "Formatter" takes care of the serialization,
    but it's very likely that serialization will be made by a third party
    library, so it's main task will be to offer an interface between
    the application and the different serializers.

---
+++ src/views/Formatter.hh
@@ -0,0 +1,72 @@
+#pragma once
+#include "concepts.hh"
+
+// output formatter
+// ================
+// TODO: possibly refactor a serializer class in the future
+//       It may also be possible that external dependecies
+//       are used to serialze the containers, instead of
+//       implementing our own serializers, but for now I'll
+//       make a simple one to shape the interface of the
+//       Formatter
+class Formatter {
+public:
+  Formatter(): format_{"yaml"} {}
+
+  /* serialize a Container with a transfomation function F,
+   * this function stringifies (serializes) an element
+   * in the container
+   */
+  template<typename Container, typename F>
+  void output(const Container& items, F f)
+  {
+    if (format_ == "yaml") {
+      std::cout << "---" << std::endl;
+      for (const auto& item: items)
+        std::cout << "- " << f(item) << std::endl;
+    }
+  }
+
+  /* serialize a Container, it provides a default serialization function
+   */
+  template<typename Container>
+  requires std::ranges::range<Container>
+    && std::convertible_to<typename Container::value_type, std::string>
+  void output(const Container& items)
+  {
+    output(items, [](const auto& x) { return std::string{x}; });
+  }
+
+  void output(std::pair<std::string, double> p) {
+    output<std::string, double>(p);
+  }
+
+  template<typename U, typename V>
+  requires std::convertible_to<U, std::string_view> &&
+           (std::is_arithmetic_v<V> || std::convertible_to<V, double>)
+  void output(const std::pair<U, V>& p) {
+  output(std::map<U, V>{{p.first, p.second}});
+}
+
+  template<typename K, typename V>
+  requires std::convertible_to<K, std::string_view>
+       && (std::is_arithmetic_v<V> || std::convertible_to<V, double>)
+  void output(const std::map<K, V>& m)
+  {
+    std::cout.setf(std::ios::fixed);
+    std::cout.precision(2);
+
+    if (format_ == "yaml") {
+      for (const auto& [key, value] : m) {
+        std::string_view k = key;
+        if constexpr (std::is_arithmetic_v<V>) {
+          std::cout << k << ": " << value << std::endl;
+        } else {
+          std::cout << k << ": " << static_cast<double>(value) << std::endl;
+        }
+      }
+    }
+  }
+private:
+  std::string format_;
+};
