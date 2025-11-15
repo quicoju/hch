cf8622a4f620fa27940beabc9829fbb78a8fcd93
quicoju <fjurado@cimat.mx>
2025-11-15 08:11:04 +0100

    rename the "check_in" property as "checkin_at"

--- src/Reservation.hh
+++ src/Reservation.hh
@@ -84,8 +84,8 @@ struct Reservation {
   std::string guest_id;
   std::string room_id;
   Period period;
-  std::optional<DateTime> check_in = std::nullopt;
-  std::optional<DateTime> check_out = std::nullopt;
+  std::optional<DateTime> checkin_at = std::nullopt;
+  std::optional<DateTime> checkout_at = std::nullopt;
   std::string notes;
 
 private:
