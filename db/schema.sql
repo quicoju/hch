51610bf9438f1e7b88a12f3ce57f15654f32b94c
quicoju <fjurado@cimat.mx>
2025-11-16 08:28:06 +0100

    introduce the "checkin_at" and "checkout_at" in the "schema.sql"

--- db/schema.sql
+++ db/schema.sql
@@ -34,6 +34,8 @@ CREATE TABLE IF NOT EXISTS reservations (
     guest_id INTEGER NOT NULL,
     begin_date DATE NOT NULL,
     duration_days INTEGER NOT NULL CHECK (duration_days > 0),
+    checkin_at DATETIME DEFAULT NULL,
+    checkout_at DATETIME DEFAULT NULL,
     notes TEXT NOT NULL DEFAULT "",
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY(room_id) REFERENCES rooms(id) ON DELETE CASCADE,
