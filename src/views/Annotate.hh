228156bd0b697997612c136f1d50b20cb4973912
quicoju <fjurado@cimat.mx>
2025-11-18 13:23:10 +0100

    add documentation

--- src/views/Annotate.hh
+++ src/views/Annotate.hh
@@ -5,8 +5,29 @@
 #include <string>
 #include <yaml-cpp/yaml.h>
 
+/**
+ * @brief Add "annotation" capabilities to the project.
+ *
+ * The idea behind this module is to provide non-member functions that that will
+ * allow the project to write data in a readable format (serialization) into an
+ * object, which could be a stream, file, etc.
+ *
+ * To add these capabilities,this module needs to know how to convert different
+ * types of objects into strings.
+ *
+ * Currently annotations will be made in YAML.
+ */
+
 namespace annotate {
   template<typename T>
+
+  /**
+   * @brief annotate data into an ostream object as YAML
+   *
+   * @param the ostream object that will take annotations
+   *
+   * @param the data that will be annotated
+   */
   void to(std::ostream& output, const T& data)
   {
     YAML::Node node;
@@ -14,6 +35,13 @@ namespace annotate {
     output << YAML::Dump(node);
   }
 
+  /**
+   * @brief turn data into a YAML string
+   *
+   * @param the data that is to be formatted
+   *
+   * @return the formatted YAML string
+   */
   template<typename T>
   std::string as_string(const T& data)
   {
