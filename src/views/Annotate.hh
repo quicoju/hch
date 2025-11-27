c4c79e3130acb1f62a9c4e17f644f02b6f97e7f1
quicoju <fjurado@cimat.mx>
2025-11-27 14:53:20 +0100

    re-organize code

--- src/views/Annotate.hh
+++ src/views/Annotate.hh
@@ -42,15 +42,14 @@ namespace annotate {
   {
     using namespace YAML;
     Emitter e;
-    e << Precision(10)
+    output << (e
+      << Precision(10)
       << BeginMap
       << Key << "date"    << Value << _dstr(r.date)
       << Key << "days"    << Value << r.duration.days()
       << Key << "total"   << Value << r.total
       << Key << "details" << Value << r.details
-      << EndMap;
-
-    output << e.c_str();
+      << EndMap).c_str();
   }
 
   /**
