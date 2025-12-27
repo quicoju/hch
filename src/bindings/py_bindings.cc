f3bf9275a12b9b6f34fcd97e0c29762fdbd7bec1
quicoju <fjurado@cimat.mx>
2025-12-27 08:09:40 +0100

    python bindings use native "datetime" types instead of a custom Date
    For now I had to remove the default value of "Days{1}", will add it back later.

--- src/bindings/py_bindings.cc
+++ src/bindings/py_bindings.cc
@@ -6,6 +6,37 @@
 // concepts.hh instead of relying on "Hotel.hh"
 #include "Hotel.hh"
 
+// Conversion between "chrono::year_month_day" and "datetime.date"
+namespace pybind11::detail {
+  template <>
+  struct type_caster<std::chrono::year_month_day> {
+  public:
+    PYBIND11_TYPE_CASTER(std::chrono::year_month_day, _("datetime.date"));
+
+    bool load(handle src, bool)
+    {
+      if (!src) return false;
+
+      PyDateTime_IMPORT;
+      if (!PyDate_Check(src.ptr())) return false;
+
+      int y = PyDateTime_GET_YEAR(src.ptr());
+      unsigned int m = PyDateTime_GET_MONTH(src.ptr());
+      unsigned int d = PyDateTime_GET_DAY(src.ptr());
+
+      value = std::chrono::year{y} / std::chrono::month{m} / std::chrono::day{d};
+      return true;
+    }
+
+    static handle
+    cast(const std::chrono::year_month_day& src, return_value_policy, handle)
+    {
+      PyDateTime_IMPORT;
+      return PyDate_FromDate(int(src.year()), unsigned(src.month()), unsigned(src.day()));
+    }
+  };
+}
+
 namespace py = pybind11;
 
 PYBIND11_MODULE(hch, m) {
@@ -13,19 +44,18 @@ PYBIND11_MODULE(hch, m) {
 
   // bindings the relevant "concepts"
   // ================================
-  py::class_<Date>(m, "Date")
-    .def(py::init<int, int, int>())
-    .def("__str__", [](const Date& d){ return _dstr(d); });
-
-  py::class_<Days>(m, "Days") // ...duration
-    .def(py::init<long>())
-    .def("__int__", [](const Days& d){ return d.days(); });
-
-  py::class_<Period>(m, "Period")
+py::class_<Period>(m, "Period")
     .def(py::init<Date, Days>())
-    .def_property_readonly("begin", &Period::begin)
+    .def_property_readonly("start", &Period::start)
+    .def_property_readonly("duration", &Period::duration)
     .def_property_readonly("end", &Period::end)
-    .def("__str__", [](const Period& p){ return _pstr(p); });
+    .def("intersects", &Period::intersects)
+    .def("__eq__", &Period::operator==)
+    .def("__str__", [](const Period& p){ return _pstr(p); })
+    .def("__repr__", [](const Period& p) {
+        return std::format("<Period start={} duration={} days>",
+                          _dstr(p.start()), p.duration().count());
+    });
 
   // Room
   // ====
@@ -58,7 +88,7 @@ PYBIND11_MODULE(hch, m) {
     .def("record_guest", &Hotel::record_guest)
     .def("reserve", &Hotel::reserve,
          py::arg("guest"), py::arg("room"),
-         py::arg("date") = Today, py::arg("duration") = Days{1})
+         py::arg("date"), py::arg("duration"))
     .def("cancel", &Hotel::cancel)
     .def("checkin", &Hotel::checkin)
     .def("checkout", &Hotel::checkout)
@@ -72,5 +102,5 @@ PYBIND11_MODULE(hch, m) {
     .def("room", &Hotel::room)
     .def("room_amenities", &Hotel::room_amenities)
     .def("rate_report_for", &Hotel::rate_report_for, py::arg("room"),
-         py::arg("date") = Today, py::arg("duration") = Days{1});
+         py::arg("date"), py::arg("duration"));
 }
