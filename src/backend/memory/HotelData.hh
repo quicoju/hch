d1aff752228080286ce43b840f8031b29722d604
quicoju <fjurado@cimat.mx>
2025-12-27 20:52:56 +0100

    rename "Date_" type in the "HotelData"
    As a reminder that the backend types don't necessarily need to match the
    implementation types.

--- src/backend/memory/HotelData.hh
+++ src/backend/memory/HotelData.hh
@@ -52,35 +52,24 @@ struct GuestData{
   std::string id;
 };
 using GuestsData = std::vector<GuestData>;
-// XXX: this isn't in the concepts because
-// I can't think of an immediate case where
-// we want to access to multiple guests at one;
-// except for a generic searching of guests, but
-// this is not to be implemented soon.
-//using Guests = std::vector<Guest>;
 
 /* Reservation information
  * =======================
  */
-using Date_ = std::chrono::year_month_day;
+using YMD = std::chrono::year_month_day;
 using DateTime = std::chrono::system_clock::time_point;
 using Duration = std::chrono::days;
 
 struct ReservationData{
   ReservationData(
-   string_view id, string_view g, string_view r, Date_ d, Duration dur, string_view n="")
+   string_view id, string_view g, string_view r, YMD d, Duration dur, string_view n="")
     : id{id}, guest_id{g}, room_id{r}, start{d}, dur{dur}, notes{n}
   {}
   std::string id;       // client facing reservation id
   std::string guest_id;
   std::string room_id;
-  Date_ start;
+  YMD start;
   Duration dur;
-  // TODO: add a rate snapshot that captures
-  // the rates at the time of the resrvation,
-  // don't want to charge a different rate
-  // if rates change between the reservation
-  // and the check-out date.
   std::string notes;
   std::optional<DateTime> checkin_at = std::nullopt;
   std::optional<DateTime> checkout_at = std::nullopt;
