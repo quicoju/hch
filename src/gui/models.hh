a0422c03e9224eeeb7d71c2bda45112b22db3d80
quicoju <fjurado@cimat.mx>
2026-03-08 22:26:31 +0100

    move the reservation table model to a "models" module
    The models might be reused by different widgets so it's better to keep them in a
    single module.

---
+++ src/gui/models.hh
@@ -0,0 +1,20 @@
+#include <QAbstractTableModel>
+
+#include "Hotel.hh"
+
+class ReservationModel : public QAbstractTableModel {
+  Q_OBJECT
+
+public:
+  explicit ReservationModel(QObject *parent = nullptr);
+  explicit ReservationModel(Reservations &&, QObject *parent = nullptr);
+
+  int rowCount(const QModelIndex &parent = QModelIndex{}) const override;
+  int columnCount(const QModelIndex &parent = QModelIndex{}) const override;
+
+  QVariant data(const QModelIndex &, int role = Qt::DisplayRole) const override;
+  QVariant headerData(int, Qt::Orientation, int role = Qt::DisplayRole) const override;
+
+private:
+  Reservations reservations;
+};
